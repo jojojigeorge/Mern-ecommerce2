a45c68cb1170765b192f8995eee0a022f24c51fb
jojojigeorge <jojojigeorge@gmail.com>
2024-05-22 14:26:14 +0530

    update vite.config file

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
       "/api": {
@@ -12,7 +10,22 @@ export default defineConfig({
         secure: false,
       },
     },
-    host:true,
-    port:5173
   },
+  // some other configuration
 })
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: 'https://server-mern-ecommerce2.vercel.app/',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//     host:true,
+//     port:5173
+//   },
+// })
