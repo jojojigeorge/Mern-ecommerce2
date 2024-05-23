60a4ee56129c03a8d37fb53654ab70dcf5be5c7a
jojojigeorge <jojojigeorge@gmail.com>
2024-05-23 13:09:55 +0530

    change vitecongig

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+
+
 export default defineConfig({
+  base: 'https://server-mern-ecommerce2.vercel.app/',
   server: {
     proxy: {
       "/api": {
